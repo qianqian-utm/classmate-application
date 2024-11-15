1e2a38c8bb8655f6e4594ab887ff346e73e5d262
Qian Qian Lim <sp-qian.qian.lim@graduate.utm.my>
2024-11-15 23:35:13 +0800

    Add additional checking to prevent error after account is removed

--- resources/views/Lecturer/index.blade.php
+++ resources/views/Lecturer/index.blade.php
@@ -8,9 +8,13 @@
                 <div class="card-header">{{ __('Lecturer Dashboard') }}</div>
 
                 <div class="card-body">
-                    <h4>Welcome, {{ $lecturer->name }}!</h4>
-                    <p>Your Email: {{ $lecturer->email }}</p>
-                    <!-- Add more lecturer-specific content here -->
+                     @if(is_null($lecturer))
+                        <p>Your account has been removed. Contact the administrator for further assisstance.</p>
+                    @else
+                        <h4>Welcome, {{ $lecturer->name }}!</h4>
+                        <p>Your Email: {{ $lecturer->email }}</p>
+                        <!-- Add more lecturer-specific content here -->
+                    @endif
                 </div>
             </div>
         </div>
--- resources/views/Student/index.blade.php
+++ resources/views/Student/index.blade.php
@@ -5,12 +5,16 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">{{ __('student Dashboard') }}</div>
+                <div class="card-header">{{ __('Student Dashboard') }}</div>
 
                 <div class="card-body">
-                    <h4>Welcome, {{ $student->name }}!</h4>
-                    <p>Your Email: {{ $student->email }}</p>
-                    <!-- Add more lecturer-specific content here -->
+                    @if(is_null($student))
+                        <p>Your account has been removed. Contact the administrator for further assisstance.</p>
+                    @else
+                        <h4>Welcome, {{ $student->name }}!</h4>
+                        <p>Your Email: {{ $student->email }}</p>
+                        <!-- Add more student-specific content here -->
+                    @endif
                 </div>
             </div>
         </div>
