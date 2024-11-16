572f6215c6de1927ef7835533a513b18e004ea3d
Qian Qian Lim <sp-qian.qian.lim@graduate.utm.my>
2024-11-16 16:45:38 +0800

    CM-34 enable google login for students

---
+++ app/Http/Controllers/GoogleLoginController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Laravel\Socialite\Facades\Socialite;
+use Illuminate\Support\Facades\Auth;
+
+class GoogleLoginController extends Controller
+{
+    public function redirectToGoogle()
+    {
+        return Socialite::driver('google')->redirect();
+    }
+
+
+    public function handleGoogleCallback()
+    {
+        $googleUser = Socialite::driver('google')->stateless()->user();
+        $user = User::where('email', $googleUser->email)->first();
+        if(!$user)
+        {
+            $user = User::create(['name' => $googleUser->name, 'email' => $googleUser->email, 'password' => \Hash::make(rand(100000,999999)), 'status' => false, 'role' => '3']);
+        }
+
+        Auth::login($user);
+
+        return Redirect()->route('home');
+        // return redirect(RouteServiceProvider::HOME);
+    }
+}
